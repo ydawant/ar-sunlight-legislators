5f7e975d03ce90c6a1b86b80ee514f574183266d
Yannick Dawant <ydawant@gmail.com>
2013-05-11 11:04:02 -0500

    test

--- app/main.rb
+++ app/main.rb
@@ -4,7 +4,7 @@ require 'active_record'
 
 def find_by_state(state)
 	puts "Senators:"
-	p Politician.find_all_by_state(state).each {find_all_by_title("Sen")}
+	students = Politician.find_all_by_state(state)and(title)
 	puts "Reps:"
 	p Politician.find_all_by_state(state).each {find_all_by_title("Rep")}
 end
