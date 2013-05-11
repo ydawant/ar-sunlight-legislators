96868f5a2f3486f520d81d592378c3f6d409e614
Yannick Dawant <ydawant@gmail.com>
2013-05-11 14:27:39 -0500

    first attempt

--- app/twitter.rb
+++ app/twitter.rb
@@ -1,4 +1,6 @@
 require 'twitter'
+require_relative 'models/politician.rb'
+require_relative '../db/config.rb'
 
 Twitter.configure do |config|
   config.consumer_key = 'th1Af7IfHEjqT6HsMAgA'
@@ -7,5 +9,13 @@ Twitter.configure do |config|
   config.oauth_token_secret = 'dP86FJneva2gEYdsrGz91aVVAN25sfVUotZIyaFKro'
 end
 
+person = Politician.find_by_name('Todd  Young ')
+twitter_handle = person.twitter_id
+twitter_id = Twitter.user(twitter_handle).id
 
- Twitter.search('RepToddYoung',:result_type => "recent")
+tweets = Twitter.user_timeline(twitter_id, :count => 10)
+
+tweets.each do |tweet| 
+	puts "#{tweet.created_at}: #{tweet.text}"
+end
+
